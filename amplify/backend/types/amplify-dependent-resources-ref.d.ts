173e590ab5b084a95b1adcf3e42f6d1504366ef7
quingsley <quingsley@hotmail.com>
2022-12-14 18:32:43 +0300

    added the trip card and functionality to add trips

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "tripsplannerresource": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
